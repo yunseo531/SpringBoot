1364bc819f0ebeda104f218a9441aa6b270910b9
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-03-21 00:07:21 +0900

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -1,11 +1,11 @@
 package com.mysite.sbb.user;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.dao.DataIntegrityViolationException;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -25,34 +25,38 @@ public class UserController {
 	
 	// /user/signup URL이 POST로 요청되면 회원가입 진행
 	@PostMapping("/signup")
-	public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
-		if(bindingResult.hasErrors()) {
-			return "signup_form";
-		}
-		
-		if(!userCreateForm.getPassword1().equals(userCreateForm.getPassword2()))
+	public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult)
+	{
+		if (bindingResult.hasErrors())
 		{
-			bindingResult.rejectValue("password2", "passwordInCorrect",
-					"2개의 비밀번호가 일치하지 않습니다.");
+			System.out.println("입력값 검증 중 에러 발생");
 			return "signup_form";
 		}
-		
-		try {
-			userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
-		}catch(DataIntegrityViolationException e) {
-			e.printStackTrace();
-			bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+		if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2()))
+		{
+			bindingResult.rejectValue("password2", "passwordInCorrect", "2개의 비밀번호가 일치하지 않습니다");
 			return "signup_form";
-		}catch(Exception e) {
+		}
+		try
+		{
+			userService.create(userCreateForm.getUsername(), userCreateForm.getPassword1(), userCreateForm.getEmail());
+		} catch (DataIntegrityViolationException e)
+		{
+			e.printStackTrace();
+			bindingResult.reject("signupFailed", "이미 등록된 사용자입니다");
+			return "signup_form";
+		} catch (Exception e)
+		{
 			e.printStackTrace();
 			bindingResult.reject("signupFailed", e.getMessage());
 			return "signup_form";
 		}
 		return "redirect:/";
 	}
-	
+
 	@GetMapping("/login")
-	public String login() {
+	public String login()
+	{
 		return "login_form";
 	}
 }
