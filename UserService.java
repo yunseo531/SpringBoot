d1e4a88723b391230f4e1d4248affbb87e5c7206
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-03-21 00:09:02 +0900

    Update UserService.java

--- UserService.java
+++ UserService.java
@@ -1,7 +1,11 @@
 package com.mysite.sbb.user;
 
+import java.util.Optional;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,13 +14,27 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
-	
-	public SiteUser create(String username, String email, String password) {
+
+	public SiteUser create(String username, String password, String email) 
+	{
 		SiteUser user = new SiteUser();
 		user.setUsername(username);
 		user.setEmail(email);
 		user.setPassword(passwordEncoder.encode(password));
 		this.userRepository.save(user);
 		return user;
+	}
+	
+	public SiteUser getUser(String username)
+	{
+		Optional<SiteUser> siteUser = this.userRepository.findByUsername(username);
+		if(siteUser.isPresent())
+		{
+			return siteUser.get();
+		}
+		else
+		{
+			throw new DataNotFoundException("siteuser not found");
+		}
 	}
 }
