5453587fac068f28eb3fe14e69eda2caff47c555
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-03-21 00:05:27 +0900

    Add files via upload

---
+++ QuestionService.java
@@ -0,0 +1,62 @@
+package com.mysite.sbb.question;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+
+import com.mysite.sbb.DataNotFoundException;
+import com.mysite.sbb.user.SiteUser;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+	
+	private final QuestionRepository questionRepository;
+	
+	public Page<Question> getList(int page){
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add(Sort.Order.desc("createDate"));
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+		return this.questionRepository.findAll(pageable);
+	}
+	
+	public Question getQuestion(Integer id) {//id값으로 질문 데이터 조회
+		Optional<Question> question = this.questionRepository.findById(id);
+		if(question.isPresent()) {
+			return question.get();
+		}else {
+			throw new DataNotFoundException("question not found");
+			//DataNotFoundException 클래스는 추가로 만들어야 컴파일 오류가 발생하지 않음
+		}
+	}
+
+	public void create(String subject, String content, SiteUser user) {
+		Question q = new Question();
+		q.setSubject(subject);
+		q.setContent(content);
+		q.setCreateDate(LocalDateTime.now());
+		q.setAuthor(user);
+		this.questionRepository.save(q);
+	}
+	
+	public void modify(Question question, String subject, String content)
+	{
+		question.setSubject(subject);
+		question.setContent(content);
+		question.setModifyDate(LocalDateTime.now());
+		this.questionRepository.save(question);
+	}
+	
+	public void delete(Question question) {
+		this.questionRepository.delete(question);
+	}
+}
