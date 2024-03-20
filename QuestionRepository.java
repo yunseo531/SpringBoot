8135e07a7b25e85265f9c0320eb5fa8c356043d4
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-03-21 00:04:33 +0900

    Add files via upload

---
+++ QuestionRepository.java
@@ -0,0 +1,14 @@
+package com.mysite.sbb.question;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer>{
+	Question findBySubject(String subject); // findBy+엔티티의 속성이름(컬럼이름)  <-- JPA가 알아서
+	Question findBySubjectAndContent(String subject, String content);
+	List<Question> findBySubjectLike(String subject);
+	Page<Question> findAll(Pageable pageable);
+}
