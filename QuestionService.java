ebaf3832e5bef14fcbfd13e583aafce90994e3b5
Yunseo <158689708+yunseo531@users.noreply.github.com>
2024-03-26 14:10:19 +0900

    Update QuestionService.java

--- QuestionService.java
+++ QuestionService.java
@@ -78,7 +78,7 @@ public class QuestionService {
 			private static final long serialVersionUID = 1L;
 			@Override
 			public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
-				query.distinct(true);
+				query.distinct(true); //중복제거
 				Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
 				Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
 				Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
